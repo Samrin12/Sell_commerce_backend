18ccc39dc0087100fedca58ea8eef18146eae841
samrin12 <samrinshanjana@gmail.com>
2022-12-26 19:44:09 +0600

    repository initialized

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,13 +9,13 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
-            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
-            `${api}/users/login`,
-            `${api}/users/register`
-            // { url: /(.*)/ },
+            // { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+            // `${api}/users/login`,
+            // `${api}/users/register`
+            { url: /(.*)/ },
         ]
     });
 }
